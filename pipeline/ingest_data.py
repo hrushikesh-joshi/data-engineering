60d731da903607043cf71ab48f3681cab0ad99d7
hrushikesh-joshi <46347619+hrushikesh-joshi@users.noreply.github.com>
2026-03-17 15:27:23 +0000

    pipeline commit

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz')
+
+
+# In[5]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+df = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    dtype=dtype,
+    parse_dates=parse_dates
+)
+
+
+# In[9]:
+
+
+df
+
+
+# In[11]:
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
+
+
+# In[12]:
+
+
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+
+
+# In[13]:
+
+
+df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+
+# In[19]:
+
+
+df_iter = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator=True,
+    chunksize=100000
+)
+
+
+# In[15]:
+
+
+for df_chunk in df_iter:
+    print(len(df_chunk))
+
+
+# In[20]:
+
+
+from tqdm.auto import tqdm
+for df_chunk in tqdm(df_iter):
+    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+
+# In[ ]:
+
+
+
+
